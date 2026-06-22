4f92319516f5724a48667eaffc5e0aec8dfdffbe
Rugsoft <futuroelectronico@gmail.com>
2026-06-22 10:35:46 +0200

    añadido footer

--- index.php
+++ index.php
@@ -34,6 +34,13 @@
         </div>
     </div>
 
+    <!-- Pie de página (Footer) -->
+        <footer class="contenedor__pie">
+            <h2 class="contenedor__pie-titulo">Biblioteca Digital</h2>
+            <p class="contenedor__pie-lema">Cultivando el conocimiento, página a página.</p>
+            <p class="contenedor__pie-copyright">&copy; <?php echo date("Y"); ?> Gestión de Libros. Todos los derechos reservados.</p>
+        </footer>
+
     <script src="script.js"></script>
 </body>
 </html>
